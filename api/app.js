0e682a90e5673ca1ac1ba45199c4d0b60b92fef7
Martin Lupa <martinignaciolupa@gmail.com>
2022-11-09 14:32:23 +0100

    mqtt handler update

--- api/app.js
+++ api/app.js
@@ -2,50 +2,26 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT;
-const { SerialPort } = require("serialport");
-const { ReadlineParser } = require("@serialport/parser-readline");
-const bodyParser = require("body-parser");
 const cors = require("cors");
+const mqttHandler = require("./mqtt_handler");
 
 //Middlewares
 app.use(cors());
 app.use(express.json());
 
-//Create a port to communicate with Arduino
-const port = new SerialPort({
-  path: "COM6",
-  baudRate: 9600,
-  dataBits: 8,
-  priority: "none",
-  stopBits: 1,
-  flowControl: false,
+//MQTT
+const mqttClient = new mqttHandler();
+mqttClient.connect();
+
+app.post("/send-mqtt", (req, res) => {
+  mqttClient.sendMessage(req.body.message);
+  res.status(200).send("Message sent to mqtt");
 });
 
-//Initialize parser to convert raw binary data into readable information
-const parser = port.pipe(
-  new ReadlineParser({
-    delimiter: "\r\n",
-  })
-);
-
-port.pipe(parser);
-
-//Node server logic
-app.get("/health", (req, res) => {
-  try {
-    res.status(200).json({
-      data: `Connection to PORT: ${port.path} successful`,
-      error: null,
-    });
-  } catch (error) {
-    res.status(200).json({ data: null, error: error });
-  }
-});
-
+//Server
 app.post("/lights/:id", (req, res) => {
   const pinId = req.params.id;
   const { pinSignal } = req.body;
-  port.write(`${pinSignal} ${pinId}`);
   console.log(`${pinSignal} ${pinId}`);
 });
 
